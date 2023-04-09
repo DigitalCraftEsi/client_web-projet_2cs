224e0d1ab7d9379354ff80953d1f6e78aa494d0d
islam36 <jm_boumendjel@esi.dz>
2023-04-09 11:18:31 +0100

    fix map bug

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,15 +1,28 @@
-import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet'
+import { MapContainer, TileLayer, Popup, Marker, ScaleControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 export function Map() {
   return (
     <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: '100%', width: "100%" }} >
+      <ScaleControl />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
+      <Marker position={[51.505, -0.09]}  >
+        <Popup offset={new L.point(12,10)}>
           A pretty CSS3 popup Easily customizable.
         </Popup>
       </Marker>
