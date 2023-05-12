dff56ae1bbc1a037fd9e3bb22413c2ad0b25d59c
Lamine-br <jl_brahami@esi.dz>
2023-05-12 14:18:47 +0100

    Profile change

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,44 +1,84 @@
+import Form from "./Form";
 import { useState, useEffect } from "react";
+import axios from "axios";
+import { BACKEND_URL } from "../util/constants";
 
 export function Profile() {
+	const [profileData, setProfileData] = useState({
+		lastName: "",
+		firstName: "",
+		email: "",
+		phone: "",
+		role: "",
+		password: "",
+	});
 
+	useEffect(() => {
+		const url = BACKEND_URL + "/profile";
+		axios
+			.get(url, { withCredentials: true })
+			.then((response) => {
+				console.log(response.data.data);
+				let res = response.data.data;
+				const newData = {
+					lastName: res.nomSADM,
+					firstName: res.prenomSADM,
+					email: res.emailSADM,
+					phone: res.telephoneSADM,
+					role: "admin",
+					password: "",
+				};
+				console.log(newData);
+				setProfileData(newData);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}, [profileData]);
 
-    const [user, setUser] = useState({
-        nom: "Berkane",
-        prenom: "Chamsou",
-        id: 1,
-        email: "chamsou_sadm@esi.dz",
-        role: "SADM",
-        telephone: "0123456789"
-    });
-
-    useEffect(() => {
-        const data = JSON.parse(localStorage.getItem("user"));
-        if(data != null) {
-            setUser(data);
-        }
-    }, [])
-
+	const handleSubmit = (newInputs) => {
+		setProfileData({
+			...profileData,
+			lastName: newInputs[0],
+			firstName: newInputs[1],
+			email: newInputs[2],
+			phone: newInputs[3],
+			role: newInputs[4],
+			password: newInputs[5],
+		});
+	};
 
 	return (
-		<div className='w-full p-10 flex flex-col gap-8'>
+		<div className='w-full p-10 flex flex-col gap-12'>
 			<h1 className='text-2xl font-bold'>My profile</h1>
 
 			<div className='bg-gray-50 flex flex-col gap-4 w-full p-4 rounded-md border-solid border-2'>
 				<h3 className='text-lg font-semibold'>
 					Informations
-					{/* <Form data={profileData} onSubmit={handleSubmit} /> */}
+					<Form data={profileData} onSubmit={handleSubmit} />
 				</h3>
 
-                <div className="w-full grid grid-cols-2" >
-                    <div className="col-span-1 font-medium" >Nom: {user.nom}</div>
-                    <div className="col-span-1 font-medium" >Prenom: {user.prenom}</div>
-                    <div className="col-span-1 font-medium" >Email: {user.email}</div>
-                    <div className="col-span-1 font-medium" >Telephone: {user.telephone}</div>
-                    <div className="col-span-1 font-medium" >Role: {user.role}</div>
-                    <div className="col-span-1 font-medium" >Password: *******</div>
-                </div>
-            </div>
-        </div>
-    );
+				<div className='w-full grid grid-cols-2'>
+					<div className='col-span-1 font-normal mb-1'>
+						Nom : {profileData.lastName}
+					</div>
+					<div className='col-span-1 font-normal mb-1'>
+						Prenom : {profileData.firstName}
+					</div>
+					<div className='col-span-1 font-normal mb-1'>
+						Email : {profileData.email}
+					</div>
+					<div className='col-span-1 font-normal mb-1'>
+						Telephone : {profileData.phone}
+					</div>
+					<div className='col-span-1 font-normal mb-1'>
+						Role : {profileData.role}
+					</div>
+					<div className='col-span-1 font-normal mb-1'>
+						Password : (not changed 69 days ago)
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
