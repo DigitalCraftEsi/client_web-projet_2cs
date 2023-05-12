6fa524cfbd3c9d7c78cfd747a7b90e1fde06b9b1
Lamine-br <jl_brahami@esi.dz>
2023-05-12 16:33:03 +0100

    Profile integrated

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from "react";
 import Button from "./Button/Button";
+import { FaEdit } from "react-icons/fa";
 
 function Form({ data, onSubmit }) {
 	const labels = Object.keys(data).map(
 		(label) => label.charAt(0).toUpperCase() + label.slice(1)
 	);
-	const initialInputs = Object.values(data);
 	const [showForm, setShowForm] = useState(false);
-	const [inputs, setInputs] = useState(initialInputs);
-	const [originalInputs, setOriginalInputs] = useState(initialInputs);
+	const [inputs, setInputs] = useState(Object.values(data));
 
 	useEffect(() => {
-		setOriginalInputs(initialInputs);
-	}, [data, initialInputs]);
+		setInputs(Object.values(data));
+	}, [data]);
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
@@ -27,29 +26,29 @@ function Form({ data, onSubmit }) {
 	};
 
 	const toggleForm = () => {
-		setInputs(originalInputs);
 		setShowForm(!showForm);
 	};
 
 	const inputFields = labels.map((label, index) => {
 		const id = `input-${index}`;
 		return (
-			<div key={id} className='grid grid-cols-2 gap-4 mb-2'>
-				<label className='block text-gray-700 font-bold mb-2'>{label}</label>
-				<input
-					className='appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-					id={id}
-					type='text'
-					value={inputs[index]}
-					onChange={(e) => handleInputChange(e, index)}
-				/>
+			<div key={id} className='grid grid-cols-2 gap-4 my-4'>
+				<label className='block text-gray-700 font-bold'>{label}</label>
+				<div className='flex'>
+					{
+						<input
+							className='appearance-none border rounded w-full py-2 px-3 h-8 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+							id={id}
+							type='text'
+							value={inputs[index]}
+							onChange={(e) => handleInputChange(e, index)}
+							disabled={label === "Role"}
+						/>
+					}
+				</div>
 			</div>
 		);
 	});
-
-	const hasChanged = inputs.some(
-		(value, index) => value !== originalInputs[index]
-	);
 
 	return (
 		<div className='inline'>
@@ -57,7 +56,7 @@ function Form({ data, onSubmit }) {
 				className='text-success underline hover:text-blue-700 ml-3 text-sm'
 				onClick={toggleForm}
 			>
-				{showForm ? "Annuler" : "Modifier"}
+				{showForm ? "Annuler" : <FaEdit size={12} />}
 			</button>
 			{showForm && (
 				<div className='fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center'>
@@ -68,11 +67,7 @@ function Form({ data, onSubmit }) {
 
 							<div className='flex items-center justify-between'>
 								<Button contenu='Annuler' type='button' onclick={toggleForm} />
-								<Button
-									contenu='Sauvegarder'
-									type='submit'
-									disabled={!hasChanged}
-								/>
+								<Button contenu='Sauvegarder' type='submit' />
 							</div>
 						</form>
 					</div>
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,60 +1,78 @@
 import Form from "./Form";
 import { useState, useEffect } from "react";
-import axios from "axios";
-import { BACKEND_URL } from "../util/constants";
+import { axiosInsance } from "../util/axios";
 
 export function Profile() {
 	const [profileData, setProfileData] = useState({
-		lastName: "",
-		firstName: "",
+		nom: "",
+		prenom: "",
 		email: "",
-		phone: "",
+		telephone: "",
+		role: "",
 	});
 
-	const [submitExecuted, setSubmitExecuted] = useState(false);
+	const password = {
+		oldPassword: "",
+		newPassword: "",
+	};
+
+	async function getProfile() {
+		const response = await axiosInsance.get(`/profile`);
+		if (response.data.statusCode === 200) {
+			let newData = {
+				nom: response.data.data.nomSADM,
+				prenom: response.data.data.prenomSADM,
+				email: response.data.data.emailSADM,
+				telephone: response.data.data.telephoneSADM,
+				role: JSON.parse(localStorage.getItem("user")).role,
+			};
+			setProfileData(newData);
+		}
+	}
+
+	async function updateProfile(inputs) {
+		let fName = inputs[1];
+		let lName = inputs[0];
+		let email = inputs[2];
+		let phone = inputs[3];
+
+		const response = await axiosInsance.post(`/profile`, {
+			fName,
+			lName,
+			email,
+			phone,
+		});
+
+		if (response.data.statusCode === 200) {
+			console.log(response.data.data);
+			let newData = {
+				...profileData,
+				nom: fName,
+				prenom: lName,
+				email: email,
+				telephone: phone,
+			};
+			setProfileData(newData);
+		}
+	}
+
+	async function updatePassword(inputs) {
+		let oldPassword = inputs[0];
+		let newPassword = inputs[1];
+
+		const response = await axiosInsance.post(`/profile/updatePassword`, {
+			oldPassword,
+			newPassword,
+		});
+
+		if (response.data.statusCode === 200) {
+			console.log(response.data.data);
+		}
+	}
 
 	useEffect(() => {
-		const url = BACKEND_URL + "/profile";
-		axios
-			.get(url, { withCredentials: true })
-			.then((response) => {
-				console.log(response.data.data);
-				let res = response.data.data;
-				const newData = {
-					lastName: res.nomSADM,
-					firstName: res.prenomSADM,
-					email: res.emailSADM,
-					phone: res.telephoneSADM,
-				};
-				console.log(newData);
-				setProfileData(newData);
-			})
-			.catch((error) => {
-				console.error(error);
-			});
-	}, [submitExecuted]);
-
-	const handleSubmit = (newInputs) => {
-		const url = BACKEND_URL + "/profile";
-		axios
-			.post(
-				url,
-				{
-					lName: newInputs[0],
-					fName: newInputs[1],
-					email: newInputs[2],
-					phone: newInputs[3],
-				},
-				{ withCredentials: true }
-			)
-			.then((response) => {
-				alert("done");
-				setSubmitExecuted(true);
-			})
-			.catch((error) => {
-				console.error(error);
-			});
-	};
+		getProfile();
+	}, []);
 
 	return (
 		<div className='w-full p-10 flex flex-col gap-12'>
@@ -63,26 +81,28 @@ export function Profile() {
 			<div className='bg-gray-50 flex flex-col gap-4 w-full p-4 rounded-md border-solid border-2'>
 				<h3 className='text-lg font-semibold'>
 					Informations
-					<Form data={profileData} onSubmit={handleSubmit} />
+					<Form data={profileData} onSubmit={updateProfile} />
 				</h3>
 
 				<div className='w-full grid grid-cols-2'>
 					<div className='col-span-1 font-normal mb-1'>
-						Nom : {profileData.lastName}
+						Nom : {profileData.nom}
 					</div>
 					<div className='col-span-1 font-normal mb-1'>
-						Prenom : {profileData.firstName}
+						Prenom : {profileData.prenom}
 					</div>
 					<div className='col-span-1 font-normal mb-1'>
 						Email : {profileData.email}
 					</div>
 					<div className='col-span-1 font-normal mb-1'>
-						Telephone : {profileData.phone}
+						Telephone : {profileData.telephone}
 					</div>
-					<div className='col-span-1 font-normal mb-1'>Role : admin</div>
+					<div className='col-span-1 font-normal mb-1'>
+						Role : {profileData.role}{" "}
+					</div>
 					<div className='col-span-1 font-normal mb-1'>
 						Password
-						{/* <Form data={password} onSubmit={handleSubmitPassword} /> */}
+						<Form data={password} onSubmit={updatePassword} />
 					</div>
 				</div>
 			</div>
